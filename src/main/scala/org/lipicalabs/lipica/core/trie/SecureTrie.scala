c3257d663888efce7010b22845b6df075e1d7f1d
YANAGISAWA, Kentaro <yanagisawa.kentaro@gmail.com>
2015-11-04 20:51:20 +0900

    Created SecureTrie class.

---
+++ src/main/scala/org/lipicalabs/lipica/core/trie/SecureTrie.scala
@@ -0,0 +1,15 @@
+package org.lipicalabs.lipica.core.trie
+
+import org.lipicalabs.lipica.core.datasource.KeyValueDataSource
+import org.lipicalabs.lipica.core.utils.{Value, ImmutableBytes}
+
+/**
+ * Created by IntelliJ IDEA.
+ * 2015/11/04 20:49
+ * YANAGISAWA, Kentaro
+ */
+class SecureTrie(_db: KeyValueDataSource, _root: Value) extends TrieImpl(_db, _root) {
+
+
+
+}
