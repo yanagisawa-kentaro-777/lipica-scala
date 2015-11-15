4a31f3a8c8c935aabfc6e1fb530ec86acc0475c2
YANAGISAWA, Kentaro <yanagisawa.kentaro@gmail.com>
2015-11-16 08:05:19 +0900

    Added a test case to ImmutableBytesTest.

--- src/test/scala/org/lipicalabs/lipica/core/utils/ImmutableBytesTest.scala
+++ src/test/scala/org/lipicalabs/lipica/core/utils/ImmutableBytesTest.scala
@@ -95,4 +95,15 @@ sequential
 			test mustEqual test
 		}
 	}
+
+	"concatenation" should {
+		"be right" in {
+			val bytes1 = ImmutableBytes(Array[Byte](0, 1, 2, 3))
+			val bytes2 = ImmutableBytes(Array[Byte](4, 5, 6, 7))
+
+			(bytes1 ++ bytes2) mustEqual ImmutableBytes(Array[Byte](0, 1, 2, 3, 4, 5, 6, 7))
+			(bytes1 :+ 4) mustEqual ImmutableBytes(Array[Byte](0, 1, 2, 3, 4))
+			(3.asInstanceOf[Byte] +: bytes2) mustEqual ImmutableBytes(Array[Byte](3, 4, 5, 6, 7))
+		}
+	}
 }
