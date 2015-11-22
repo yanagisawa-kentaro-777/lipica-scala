9312cb2d10995ac533eaa626828533c128925ed0
YANAGISAWA, Kentaro <yanagisawa.kentaro@gmail.com>
2015-11-22 11:12:19 +0900

    Implemented ImportResult.

---
+++ src/main/scala/org/lipicalabs/lipica/core/ImportResult.scala
@@ -0,0 +1,29 @@
+package org.lipicalabs.lipica.core
+
+/**
+ * Created by IntelliJ IDEA.
+ * 2015/11/22 11:09
+ * YANAGISAWA, Kentaro
+ */
+sealed trait ImportResult {
+	def isSuccessful: Boolean
+}
+
+object ImportResult {
+	case object ImportedBest extends ImportResult {
+		override val isSuccessful: Boolean = true
+	}
+	case object ImportedNotBest extends ImportResult {
+		override val isSuccessful: Boolean = true
+	}
+	case object Exists extends ImportResult {
+		override val isSuccessful: Boolean = false
+	}
+	case object NoParent extends ImportResult {
+		override val isSuccessful: Boolean = false
+	}
+	case object ConsensusBreak extends ImportResult {
+		override val isSuccessful: Boolean = false
+	}
+
+}
