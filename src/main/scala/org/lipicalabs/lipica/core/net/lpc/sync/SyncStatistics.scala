4c982263f68b464ef9c4dde2b7435041489f52c3
YANAGISAWA, Kentaro <yanagisawa.kentaro@gmail.com>
2015-12-12 15:35:39 +0900

    Implemented SyncStatistics class.

--- src/main/scala/org/lipicalabs/lipica/core/net/lpc/sync/SyncStatistics.scala
+++ src/main/scala/org/lipicalabs/lipica/core/net/lpc/sync/SyncStatistics.scala
@@ -6,10 +6,42 @@ package org.lipicalabs.lipica.core.net.lpc.sync
  * YANAGISAWA, Kentaro
  */
 class SyncStatistics {
-	//TODO 未実装。
 
-	def reset(): Unit = ???
+	private var updatedAt: Long = 0L
+	private var _blocksCount: Long = 0L
+	def blocksCount: Long = this._blocksCount
 
-	def addBlocks(v: Long): Unit = ???
-	def addHashes(v: Long): Unit = ???
+	private var _hashesCount: Long = 0L
+	def hashesCount: Long = this._hashesCount
+
+	private var _emptyResponsesCount: Int = 0
+	def emptyResponsesCount: Int = this._emptyResponsesCount
+
+	def reset(): Unit = {
+		this.updatedAt = System.currentTimeMillis
+		this._blocksCount = 0
+		this._hashesCount = 0
+		this._emptyResponsesCount = 0
+	}
+
+	def addBlocks(v: Long): Unit = {
+		this._blocksCount += v
+		fixCommon(v)
+	}
+
+	def addHashes(v: Long): Unit = {
+		this._hashesCount += v
+		fixCommon(v)
+	}
+
+	private def fixCommon(v: Long): Unit = {
+		if (v == 0) {
+			this._emptyResponsesCount += 1
+		}
+		this.updatedAt = System.currentTimeMillis
+	}
+
+	def millisSinceLastUpdate: Long = System.currentTimeMillis - this.updatedAt
+
+	reset()
 }
