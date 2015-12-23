e3fbcff39fc3270861daffd759a96706c96bbf2d
YANAGISAWA, Kentaro <yanagisawa.kentaro@gmail.com>
2015-12-23 13:52:02 +0900

    Implemented ChannelInitializer class.

--- src/main/scala/org/lipicalabs/lipica/core/net/server/LipicaChannelInitializer.scala
+++ src/main/scala/org/lipicalabs/lipica/core/net/server/LipicaChannelInitializer.scala
@@ -1,7 +1,9 @@
 package org.lipicalabs.lipica.core.net.server
 
-import io.netty.channel.ChannelInitializer
+import io.netty.channel._
 import io.netty.channel.socket.nio.NioSocketChannel
+import org.lipicalabs.lipica.core.manager.WorldManager
+import org.slf4j.LoggerFactory
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,9 +11,44 @@ import io.netty.channel.socket.nio.NioSocketChannel
  * YANAGISAWA, Kentaro
  */
 class LipicaChannelInitializer(val remoteId: String) extends ChannelInitializer[NioSocketChannel] {
-	//TODO 未実装。
-	override def initChannel(c: NioSocketChannel) = ???
 
-	def peerDiscoveryMode: Boolean = ???
-	def peerDiscoveryMode_=(v: Boolean): Unit = ???
+	import LipicaChannelInitializer._
+
+	//TODO auto wiring
+	private val channelManager: ChannelManager = null
+	private val worldManager: WorldManager = null
+
+	private var _peerDiscoveryMode: Boolean = false
+	def peerDiscoveryMode: Boolean = this._peerDiscoveryMode
+	def peerDiscoveryMode_=(v: Boolean): Unit = this._peerDiscoveryMode = v
+
+	override def initChannel(ch: NioSocketChannel): Unit = {
+		try {
+			logger.info("<ChannelInitializer> Opening connection: %s".format(ch))
+			val channel = new Channel
+			channel.init(ch.pipeline, this.remoteId, this.peerDiscoveryMode)
+			if (!peerDiscoveryMode) {
+				this.channelManager.add(channel)
+			}
+			ch.config.setRecvByteBufAllocator(new FixedRecvByteBufAllocator(16777216))
+			ch.config.setOption(ChannelOption.SO_RCVBUF, Integer.valueOf(16777216))
+			ch.config.setOption(ChannelOption.SO_BACKLOG, Integer.valueOf(1024))
+
+			ch.closeFuture.addListener(new ChannelFutureListener {
+				override def operationComplete(f: ChannelFuture) = {
+					if (!peerDiscoveryMode) {
+						channelManager.notifyDisconnect(channel)
+					}
+				}
+			})
+		} catch {
+			case e: Exception =>
+				logger.warn("<ChannelInitializer> Exception caught: %s".format(e.getClass.getSimpleName), e)
+		}
+	}
+
+}
+
+object LipicaChannelInitializer {
+	private val logger = LoggerFactory.getLogger("net")
 }
